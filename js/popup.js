5e309cfd481bbe172afaf52b0c47d683b304c644
Diego Fleitas <DiegoFleitas@users.noreply.github.com>
2023-10-02 15:49:02 -0300

    Release/release candidate 1.1.0 (#2)
    * add .gitignore
    
    * add domain specific searches
    
    * Release v1.1.0
    
    * update manifest

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,11 @@
 document.getElementById("suggest").addEventListener("click", () => {
-  chrome.storage.local.get("query", (data) => {
-    let query = data.query;
-    if (query) {
+  chrome.storage.local.get(["offendingDomains", "originalQuery"], (data) => {
+    let offendingDomains = data.offendingDomains;
+    let originalQuery = data.originalQuery;
+    if (offendingDomains && offendingDomains.length > 0 && originalQuery) {
+      let query = `${originalQuery} domain=${offendingDomains.join(
+        " OR domain="
+      )}`;
       let yandexUrl =
         "https://yandex.com/search/?text=" + encodeURIComponent(query);
       chrome.tabs.create({ url: yandexUrl });
