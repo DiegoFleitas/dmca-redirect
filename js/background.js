daf39324d73b59625d51852618e5c4339aeda5e6
Diego Fleitas <diego.fleitas68@gmail.com>
2023-10-02 15:43:13 -0300

    add domain specific searches

--- js/background.js
+++ js/background.js
@@ -1,38 +1,64 @@
+import { getOffendingDomains, getLumenUrls, extractQuery } from "./utils.js";
 console.log("Background script loaded.");
 
-const extractQuery = (url) => {
-  let match = url.match(/[?&]q=([^&]*)/);
-  return match ? decodeURIComponent(match[1]) : null;
-};
+let processedLumenUrls = new Set();
 
 // Listener for web requests
 chrome.webRequest.onCompleted.addListener(
   (details) => {
-    console.log(details); // Log the details object
     // Inject a script into the tab to check for a DMCA notice
     chrome.tabs.executeScript(
       details.tabId,
       {
         code: 'document.body.innerText.includes("DMCA")',
       },
-      (results) => {
+      async (results) => {
         if (chrome.runtime.lastError) {
           console.error(chrome.runtime.lastError);
           return;
         }
 
-        console.log(results); // Log the results object
         // Check the result of the script injection
         if (results[0]) {
           console.log("DMCA notice found, redirecting to Yandex");
-          let query = extractQuery(details.url);
-          console.log(query); // Log the extracted query
-          chrome.storage.local.set({ query: query }, () => {
+          // Extract the Lumen Database URL and original query
+          try {
+            let lumenUrls = await getLumenUrls(details.tabId);
+            let allOffendingDomains = [];
+            for (let lumenUrl of lumenUrls) {
+              if (processedLumenUrls.has(lumenUrl)) {
+                console.log("Already processed:", lumenUrl);
+                continue; // Skip this URL if it's already been processed
+              }
+              let offendingDomains = await getOffendingDomains(lumenUrl);
+              allOffendingDomains.push(...offendingDomains);
+              processedLumenUrls.add(lumenUrl); // Mark this URL as processed
+            }
+            // Save all offending domains to local storage
+            chrome.storage.local.set(
+              { offendingDomains: allOffendingDomains },
+              () => {
+                if (chrome.runtime.lastError) {
+                  console.error(chrome.runtime.lastError);
+                  return;
+                }
+                console.log("Offending domains saved:", allOffendingDomains);
+              }
+            );
+          } catch (error) {
+            console.error(
+              "Error fetching or parsing Lumen Database pages:",
+              error
+            );
+          }
+
+          let originalQuery = extractQuery(details.url);
+          chrome.storage.local.set({ originalQuery: originalQuery }, () => {
             if (chrome.runtime.lastError) {
               console.error(chrome.runtime.lastError);
               return;
             }
-            console.log("Query saved:", query);
+            console.log("Query saved:", originalQuery);
           });
           // Set badge text and background color
           chrome.browserAction.setBadgeText({
