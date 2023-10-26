25dbe1ec757859038a7bf6c16f3cad66f7bae384
Diego Fleitas <diego.fleitas68@gmail.com>
2023-10-26 11:19:50 -0300

    update popup

--- js/popup.js
+++ js/popup.js
@@ -2,8 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
   chrome.storage.local.get(["offendingDomains", "originalQuery"], (data) => {
     let { offendingDomains, originalQuery } = data;
     let linksUl = document.getElementById("links");
+    linksUl.innerHTML = ""; // clear
     if (offendingDomains && offendingDomains.length > 0 && originalQuery) {
+      let headerLi = document.createElement("li");
+      headerLi.className = "list-header";
+      let formattedQuery = originalQuery.replaceAll("+", " ");
+      headerLi.textContent = `Retry search for: ${formattedQuery}`;
+      linksUl.appendChild(headerLi);
+      let addedDomains = new Set();
       offendingDomains.forEach((domain) => {
+        if (addedDomains.has(domain)) return;
+        addedDomains.add(domain);
         let query = `${originalQuery} domain=${domain}`;
         let yandexUrl =
           "https://yandex.com/search/?text=" + encodeURIComponent(query);
@@ -11,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
         let li = document.createElement("li");
         let link = document.createElement("a");
         link.href = yandexUrl;
-        link.textContent = `Retry search (${domain})`;
+        link.textContent = domain;
         link.target = "_blank"; // Open link in a new tab
         li.appendChild(link);
         linksUl.appendChild(li); // Append the list item to the unordered list
