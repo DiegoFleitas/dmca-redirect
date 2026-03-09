1b5bd9b47a69d1c4badec7da2e4a4a5afb6b7447
Diego Fleitas <diego.fleitas68@gmail.com>
2026-03-09 11:37:09 -0300

    refactor: improve variable declarations and update permissions in manifest

--- js/popup.js
+++ js/popup.js
@@ -1,24 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   chrome.storage.local.get(["offendingDomains", "originalQuery"], (data) => {
-    let { offendingDomains, originalQuery } = data;
-    let linksUl = document.getElementById("links");
+    const { offendingDomains, originalQuery } = data;
+    const linksUl = document.getElementById("links");
     linksUl.innerHTML = ""; // clear
+
     if (offendingDomains && offendingDomains.length > 0 && originalQuery) {
-      let headerLi = document.createElement("li");
+      const headerLi = document.createElement("li");
       headerLi.className = "list-header";
-      let formattedQuery = originalQuery.replaceAll("+", " ");
+      const formattedQuery = originalQuery.replaceAll("+", " ");
       headerLi.textContent = `Retry search for: ${formattedQuery}`;
       linksUl.appendChild(headerLi);
-      let addedDomains = new Set();
+
+      const addedDomains = new Set();
       offendingDomains.forEach((domain) => {
         if (addedDomains.has(domain)) return;
         addedDomains.add(domain);
-        let query = `${originalQuery} domain=${domain}`;
-        let yandexUrl =
+        const query = `${originalQuery} domain=${domain}`;
+        const yandexUrl =
           "https://yandex.com/search/?text=" + encodeURIComponent(query);
 
-        let li = document.createElement("li");
-        let link = document.createElement("a");
+        const li = document.createElement("li");
+        const link = document.createElement("a");
         link.href = yandexUrl;
         link.textContent = domain;
         link.target = "_blank"; // Open link in a new tab
@@ -28,5 +30,8 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       linksUl.textContent = "No offending domains found";
     }
+
+    // Clear stored data after it has been used in the popup
+    chrome.storage.local.remove(["offendingDomains", "originalQuery"]);
   });
 });
